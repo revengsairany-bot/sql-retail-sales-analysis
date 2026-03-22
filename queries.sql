2fbba18481adf088407cd5252c8cebd28d66895b
Reveng Sairany <revengsairany@gmail.com>
2026-03-22 16:50:14 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,16 +1,42 @@
--- Revenue by Country
+-- =========================
+-- 1. Revenue by Country
+-- =========================
 SELECT 
     Country,
     ROUND(SUM(Quantity * UnitPrice), 2) AS revenue
 FROM retail
 GROUP BY Country
-ORDER BY revenue DESC;
+ORDER BY revenue DESC
+LIMIT 10;
 
--- Top Customers by Revenue
+
+-- =========================
+-- 2. Top Customers by Revenue
+-- =========================
 SELECT 
     CustomerID,
     ROUND(SUM(Quantity * UnitPrice), 2) AS revenue
 FROM retail
+WHERE CustomerID IS NOT NULL
 GROUP BY CustomerID
 ORDER BY revenue DESC
 LIMIT 10;
+
+
+-- =========================
+-- 3. Monthly Revenue Trend
+-- =========================
+SELECT 
+    '20' || substr(
+        InvoiceDate,
+        instr(InvoiceDate, '/') + instr(substr(InvoiceDate, instr(InvoiceDate, '/') + 1), '/') + 1,
+        2
+    )
+    || '-' ||
+    printf('%02d', CAST(substr(InvoiceDate, 1, instr(InvoiceDate, '/') - 1) AS INTEGER)) AS month,
+
+    ROUND(SUM(Quantity * UnitPrice), 2) AS revenue
+
+FROM retail
+GROUP BY month
+ORDER BY month;
